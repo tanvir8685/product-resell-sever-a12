480ad37244706b87217c29ca28035a41ad0f3c66
tanvir <tanvirhridoy1011@gmail.com>
2022-11-29 01:18:48 +0600

    make fake data for categories

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 
 const app = express();
+const cors = require('cors');
+const categories=require('./data/categories.json')
+
 
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 app.get('/',(req,res)=>{
     res.send('its working')
+})
+app.get('/vehaicel-categories',(req,res)=>{
+    res.send(categories)
 })
 app.listen(port,()=>{
     console.log('its working on',port)
