c26e81b5742dacaaf6f8b1c0b3c9ed1039923dd5
tanvir <tanvirhridoy1011@gmail.com>
2022-11-29 02:26:26 +0600

    make api for single product

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const categories=require('./data/categories.json')
+const products=require('./data/producucts.json')
 
 
 const port = process.env.PORT || 5000;
@@ -14,6 +15,15 @@ app.get('/',(req,res)=>{
 app.get('/vehaicel-categories',(req,res)=>{
     res.send(categories)
 })
+
+
+app.get('/product/:id',(req,res)=>{
+    const id=req.params.id;
+    const selectedProducts=products.find(product=>product._id===id);
+    res.send(selectedProducts)
+
+})
+
 app.listen(port,()=>{
     console.log('its working on',port)
 })
