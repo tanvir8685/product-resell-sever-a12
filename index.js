c87be12f70a2fd6e8e4a1a804edb806dfb527a33
tanvir <tanvirhridoy1011@gmail.com>
2022-11-30 18:32:29 +0600

    load product from database

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
-
-const products = require('./data/producucts.json')
-
-
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -17,12 +13,6 @@ app.get('/', (req, res) => {
     res.send('its working')
 })
 
-
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    const category_product = products.filter(product => product.cat_id === id);
-    res.send(category_product)
-})
 
 app.get('/product/:id', (req, res) => {
     const id = req.params.id;
@@ -39,17 +29,36 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
-    try{
-        const categoriesOptionCollection=client.db('products-resell').collection('categories')
+    try {
+        const categoriesOptionCollection = client.db('products-resell').collection('categories')
+        const categoriesProductCollection = client.db('products-resell').collection('products')
 
-        app.get('/vehaicel-categories',async(req,res)=>{
-            const query={};
-            const categories=await categoriesOptionCollection.find(query).toArray();
+        app.get('/vehaicel-categories', async (req, res) => {
+            const query = {};
+            const categories = await categoriesOptionCollection.find(query).toArray();
             res.send(categories)
+        });
+        app.get('/category', async (req, res) => {
+            const query = {};
+            const products = await categoriesProductCollection.find(query).toArray();
+            res.send(products)
+        });
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+
+
+            const query = { cat_id: id };
+
+
+            const categorisProduct = await categoriesProductCollection.find(query).toArray();
+
+            res.send(categorisProduct)
+
         })
 
+
     }
-    finally{
+    finally {
 
     }
 }
